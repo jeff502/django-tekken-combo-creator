12946a975dbc4e6322ed12b7dc1045c54923b7b0
Jeff McElhiney <jeffmcelhineywork@gmail.com>
2024-03-16 11:56:57 -0500

    move assets

--- tekken/app/views.py
+++ tekken/app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .templates.app
 
-# Create your views here.
+
+class IndexView(View):
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+    
+
+    def post(self, request, *args, **kwargs):
+        pass
