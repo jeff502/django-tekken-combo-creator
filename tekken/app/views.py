002df7d78b3ace6b5e12eea96402289206c45aa9
Jeff McElhiney <jeffmcelhineywork@gmail.com>
2024-03-19 17:48:52 -0500

    remove _Dark.png files. Add in logic to select character

--- tekken/app/views.py
+++ tekken/app/views.py
@@ -1,18 +1,35 @@
+import glob
+import os
 from django.shortcuts import render
 from django.views.generic import View
-from .templates.app
 
 
 class IndexView(View):
     template_name = 'index.html'
+    BASE = "C:/Users/jeffm/VSC projects/tekken-8-web-app/tekken/app/static/app/"
+    assets = BASE + "assets"
+    ps = BASE + "assets_ps"
+    xbox = BASE + "assets_xbox"
+    chars = BASE + "char"
+    buttons = None
+
+    template_name = 'app/index.html'
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Home'
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['title'] = 'Home'
+    #     return context
 
     def get(self, request, *args, **kwargs):
-        context = self.get_context_data()
+        if self.buttons is None:
+            self.buttons = self.ps
+        context = {}
+        characters =  {}
+        for item in os.listdir(self.chars):
+            name = item.split(".")[0]
+            characters[name] = item
+                
+        context['characters'] = characters
         return render(request, self.template_name, context)
     
 
