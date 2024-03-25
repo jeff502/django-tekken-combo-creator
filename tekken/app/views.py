b9567814b2f20ca6aec7f0c3627074a595fbffc8
Jeff McElhiney <jeffmcelhineywork@gmail.com>
2024-03-25 12:56:50 -0500

    fix button issue

--- tekken/app/views.py
+++ tekken/app/views.py
@@ -2,56 +2,67 @@ import glob
 import os
 from django.shortcuts import render
 from django.views.generic import View
-from django.conf import settings
 
 
 class IndexView(View):
     def __init__(self):
-        self.characters = self.get_characters()
-        self.moves, self.move_type = self.get_buttons(self.xbox)
-
-    template_name = 'index.html'
-    BASE = "C:/Users/jeffm/VSC projects/tekken-8-web-app/tekken/app/static/app/"
-    assets = "assets/"
-    ps = "assets_ps/"
-    xbox = "assets_xbox/"
+        self.characters = {}
+        self.buttons = []
 
     template_name = 'app/index.html'
+    static_path = "app\\static\\app\\"
+    context = {}
+
     
 
     def get(self, request, *args, **kwargs):
-        context = {}
-        context['characters'] = self.characters
-        context["moves"] = self.moves
-        context["move_type"] = self.move_type
-        
-        print(self.moves[0])
-        return render(request, self.template_name, context)
+        if not self.context:
+            self.get_context()
+
+        return render(request, self.template_name, self.context)
     
 
     def post(self, request, *args, **kwargs):
         if request.POST:
             user_character = request.POST.get("character")
+            request.session["user_character"] = user_character
 
-        return render(request, self.template_name)
+            clear_combo = request.POST.get("clear")
+            if clear_combo:
+                request.session["combo"] = []
 
-    def get_characters(self) -> dict:
-        characters_path = self.BASE + "chararacters"
+            move = request.POST.get("move-value")
+            if move:
+                if not request.session.get("combo"):
+                    request.session["combo"] = [move]
+                else:
+                    move_list = request.session.get("combo")
+                    move_list.append(move)
+                    request.session["combo"] = move_list
+
+        return render(request, self.template_name, self.context)
+
+    def get_characters(self):
+        characters_path = self.static_path  + "characters"
         characters =  {}
         for item in os.listdir(characters_path):
             name = item.split(".")[0]
-            characters[name] = item
-                
-        characters['characters'] = characters
-        return characters
+            characters[name] = f"characters\\{item}"
+        self.characters = characters
         
-    def get_buttons(self, image_dir) -> dict:
-        static_dir = "static/app/"
-        button_path = self.BASE + image_dir
+    def get_buttons(self, image_dir="xbox"):
+        button_path = self.static_path + image_dir
         buttons = []
         for filename in os.listdir(button_path):
             if filename.endswith(('.jpg', '.jpeg', '.png')):
-                image_url = os.path.join(static_dir, image_dir, filename)
+                image_url = f"xbox\\{filename}"
                 buttons.append(image_url)
 
-        return buttons, image_dir
+        self.buttons = buttons
+
+    def get_context(self):
+        self.get_buttons()
+        self.get_characters()
+        self.context["characters"] = self.characters
+        self.context["buttons"] = self.buttons
+        self.context["button_type"] = "default"
--- tekken/tekken/settings.py
+++ tekken/tekken/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "app/static/app"
 ]
 
 # Default primary key field type
